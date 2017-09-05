20a894e964e762bbb4b6eb3933e4499c0877a0d3
Mikaeil Orfanian <mokt@outlook.com>
2017-09-05 11:07:13 +0200

    add tests for utils module

--- drf_redis_tokens/utils.py
+++ drf_redis_tokens/utils.py
@@ -1,5 +1,7 @@
 
 TOKEN_HASH_SEPARATOR = ':hash:'
+WRONG_TOKEN = 'wrong_token'
+WRONG_HASH = 'wrong_hash'
 
 
 def make_full_token(token, hash):
@@ -8,7 +10,9 @@ def make_full_token(token, hash):
 
 def parse_full_token(full_token):
     token_hash = full_token.split(TOKEN_HASH_SEPARATOR)
-    
+
+    # return bad results so verify function in crypto.py doesn't crash
+    # we don't want bad tokens to cause 500 errors on server side
     if len(token_hash) != 2:
         token_hash = ['wrong_token', 'wrong_hash']
     
